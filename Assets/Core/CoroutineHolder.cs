4a2cc7312c08bd99737aaabb75870b73667bd528
M-Knk <dev@dev-no-MacBook-Pro.local>
2018-04-20 22:05:07 +0900

    大丈夫そうなのコミット

---
+++ Assets/Core/CoroutineHolder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityUtility
+{
+	public class CoroutineHolder
+	{
+		private readonly MonoBehaviour owner;
+		private Coroutine currentCoroutine = null;
+
+		public CoroutineHolder(MonoBehaviour behavior)
+		{
+			owner = behavior;
+		}
+
+		public void Start(IEnumerator routine)
+		{
+			Stop();
+
+			currentCoroutine = owner.StartCoroutine(StartAsync(routine));
+		}
+		private IEnumerator StartAsync(IEnumerator routine)
+		{
+			yield return routine;
+			currentCoroutine = null;
+		}
+
+		public void Stop()
+		{
+			if (currentCoroutine != null)
+			{
+				owner.StopCoroutine(currentCoroutine);
+				currentCoroutine = null;
+			}
+		}
+	}
+}
---
+++ Assets/Core/Tuple.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityUtility
+{
+	public struct Tuple<T, U>
+	{
+		public T Item1 { get; private set; }
+		public U Item2 { get; private set; }
+
+		public Tuple(T item1, U item2)
+		{
+			Item1 = item1;
+			Item2 = item2;
+		}
+	}
+
+	public struct Tuple<T, U, V>
+	{
+		public T Item1 { get; private set; }
+		public U Item2 { get; private set; }
+		public V Item3 { get; private set; }
+
+		public Tuple(T item1, U item2, V item3)
+		{
+			Item1 = item1;
+			Item2 = item2;
+			Item3 = item3;
+		}
+	}
+}
