208ff1bd23225d7fe96328cda99e96e895398bcc
M-Knk <foo@bar.com>
2018-10-27 18:58:08 +0900

    シリアライズフィールドにEnumPopupを使うためのカスタムプロパティ

---
+++ Scripts/PropertyAttributes/EnumPopupAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityUtility.PropertyAttributes
+{
+	/// <summary>
+	/// EditorGUI.EnumPopup()で表示する.
+	/// </summary>
+	public class EnumPopupAttribute : PropertyAttribute
+	{
+		public readonly System.Type enumType;
+		public EnumPopupAttribute(System.Type enumType)
+		{
+			this.enumType = enumType;
+		}
+	}
+
+#if UNITY_EDITOR
+	[CustomPropertyDrawer(typeof(EnumPopupAttribute))]
+	public class EnumPopupDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EnumPopupAttribute enumPopupAttribute = (EnumPopupAttribute)attribute;
+
+			if (property.propertyType != SerializedPropertyType.Enum)
+			{
+				EditorGUI.PropertyField(position, property);
+				return;
+			}
+
+			System.Array values = System.Enum.GetValues(enumPopupAttribute.enumType);
+			System.Enum selected = (System.Enum)values.GetValue(property.enumValueIndex);
+			selected = EditorGUI.EnumPopup(position, property.displayName, selected);
+			property.enumValueIndex = System.Array.IndexOf(values, selected);
+		}
+	}
+#endif
+}
