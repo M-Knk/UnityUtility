cd173a42d427d13b932ba460442ce0dd8399f6c7
M-Knk <dev@dev-no-MacBook-Pro.local>
2019-09-21 13:13:04 +0900

    ローカルテクスチャ読み込み

---
+++ Runtime/Scripts/OutsideResource/LocalTexture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace UnityUtility.OutsideResource
+{
+	public class LocalTexture
+	{
+		public static IEnumerator GetTexture(string file, System.Action<Texture2D> onComplete)
+		{
+			UnityWebRequest www = UnityWebRequestTexture.GetTexture(file);
+			yield return www.SendWebRequest();
+
+			Texture2D myTexture = null;
+			if (www.isNetworkError || www.isHttpError)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+			}
+
+			if (onComplete != null)
+			{
+				onComplete(myTexture);
+			}
+		}
+	}
+}
