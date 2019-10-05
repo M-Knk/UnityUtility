42f17f5b84af3aea0d00cee5dd86818207945796
M-Knk <foo@bar.com>
2019-10-06 00:52:23 +0900

    IntObjectStore追加

---
+++ Runtime/Scripts/ScriptableAssets/IntObjectStore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityUtility.CommonScriptableObject
+{
+	[CreateAssetMenu]
+	public class IntObjectStore : ScriptableObject
+	{
+		[System.Serializable]
+		public class KeyValue
+		{
+			public int key;
+			public UnityEngine.Object value;
+		}
+
+		[SerializeField]
+		KeyValue[] keyValues;
+
+		public bool TryGetValue<ValueType>(int key, out ValueType value)
+			where ValueType : UnityEngine.Object
+		{
+			foreach (var keyValue in keyValues)
+			{
+				if (keyValue.key == key)
+				{
+					value = keyValue.value as ValueType;
+					if (value != null)
+					{
+						return true;
+					}
+				}
+			}
+
+			value = default(ValueType);
+			return false;
+		}
+	}
+}
